92bb002d5675a47bd69384ea2f4739ff23c5fc7c
fang-0219 <kevin_fangfang@163.com>
2021-07-04 22:41:06 +0800

    使用json-server

--- src/views/sandbox/home/Home.js
+++ src/views/sandbox/home/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import axios from 'axios'
 export default function Home() {
+    const getData = () => {
+        //json-server使用 
+        // 获取数据
+        // axios.get('http://localhost:8000/posts').then(res => console.log(res))
+        //新增
+        axios.post('http://localhost:8000/posts',{
+            title:'text',
+            author:'fizer'
+        })
+    }
     return (
         <div>
-            Home
+            <button onClick={getData}>获取数据</button>
         </div>
     )
 }
